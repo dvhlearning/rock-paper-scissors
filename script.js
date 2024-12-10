a0df66cf73e5c98d7dc6aae5020183ea892b6304
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-09 23:40:07 -0500

    wrote and tested playRound()

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const randNum = Math.random();
@@ -20,5 +21,47 @@ function getHumanChoice(){
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
+        console.log("Error: '" + humanChoice + "' is an incorrect Selection, try again.")
+    } else {
+        if (humanChoice === "rock"){
+            if (computerChoice === "rock") {
+                console.log("Tie! Scissors cancels scissors.");
+            } else if (computerChoice === "paper") {
+                console.log("You lose! Paper beats rock.");
+                computerScore++;
+            } else {
+                console.log("You win! Rock beats scissors.");
+                humanScore++;
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                console.log("You win! Paper beats rock.");
+                humanScore++;
+            } else if (computerChoice === "paper") {
+                console.log("Tie! Paper cancels paper.");
+            } else {
+                console.log("You lose! Scissors beat paper.");
+                computerScore++;
+            }
+        } else {
+            if (computerChoice === "rock") {
+                console.log("You lose! Rock beats scissors.");
+                computerScore++;
+            } else if (computerChoice === "paper") {
+                console.log("You win! Scissors beat paper.");
+                humanScore++;
+            } else {
+                console.log("Tie! Scissors cancel scissors.");
+            }
+        }
+
+        console.log("Your Score: " + humanScore);
+        console.log("Computer Score: " + computerScore);
+    }
+}
+
+playRound(getHumanChoice(),getComputerChoice());
