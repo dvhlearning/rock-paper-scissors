5d03b86826aac13ce7db0e4e3d6dfc81cccb5fce
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-09 23:09:01 -0500

    wrote and tested getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+console.log("Hello World");
+
+function getComputerChoice() {
+    const randNum = Math.random();
+    let choice;
+    if (randNum <= 0.33) {
+        choice = "rock";
+    } else if (randNum <= 0.66) {
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+
+    return choice;
+}
+
+console.log(getComputerChoice());
