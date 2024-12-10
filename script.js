6f74bf888a214f419d05e8be85645afb77224d2f
dvh <177888353+dvhlearning@users.noreply.github.com>
2024-12-10 12:53:10 -0500

    created playGame() and moved playRound() inside it

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const randNum = Math.random();
     let choice;
@@ -21,47 +18,56 @@ function getHumanChoice(){
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
-        console.log("Error: '" + humanChoice + "' is an incorrect Selection, try again.")
-    } else {
-        if (humanChoice === "rock"){
-            if (computerChoice === "rock") {
-                console.log("Tie! Scissors cancels scissors.");
-            } else if (computerChoice === "paper") {
-                console.log("You lose! Paper beats rock.");
-                computerScore++;
-            } else {
-                console.log("You win! Rock beats scissors.");
-                humanScore++;
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice === "rock") {
-                console.log("You win! Paper beats rock.");
-                humanScore++;
-            } else if (computerChoice === "paper") {
-                console.log("Tie! Paper cancels paper.");
-            } else {
-                console.log("You lose! Scissors beat paper.");
-                computerScore++;
-            }
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
+            console.log("Error: '" + humanChoice + "' is an incorrect Selection, try again.")
         } else {
-            if (computerChoice === "rock") {
-                console.log("You lose! Rock beats scissors.");
-                computerScore++;
-            } else if (computerChoice === "paper") {
-                console.log("You win! Scissors beat paper.");
-                humanScore++;
+            console.log("You selected: " + humanChoice);
+            if (humanChoice === "rock"){
+                if (computerChoice === "rock") {
+                    console.log("Tie! Scissors cancels scissors.");
+                } else if (computerChoice === "paper") {
+                    console.log("You lose! Paper beats rock.");
+                    computerScore++;
+                } else {
+                    console.log("You win! Rock beats scissors.");
+                    humanScore++;
+                }
+            } else if (humanChoice === "paper") {
+                if (computerChoice === "rock") {
+                    console.log("You win! Paper beats rock.");
+                    humanScore++;
+                } else if (computerChoice === "paper") {
+                    console.log("Tie! Paper cancels paper.");
+                } else {
+                    console.log("You lose! Scissors beat paper.");
+                    computerScore++;
+                }
             } else {
-                console.log("Tie! Scissors cancel scissors.");
+                if (computerChoice === "rock") {
+                    console.log("You lose! Rock beats scissors.");
+                    computerScore++;
+                } else if (computerChoice === "paper") {
+                    console.log("You win! Scissors beat paper.");
+                    humanScore++;
+                } else {
+                    console.log("Tie! Scissors cancel scissors.");
+                }
             }
+    
+            console.log("Your Score: " + humanScore);
+            console.log("Computer Score: " + computerScore);
         }
-
-        console.log("Your Score: " + humanScore);
-        console.log("Computer Score: " + computerScore);
     }
+
+    
+
 }
 
 playRound(getHumanChoice(),getComputerChoice());
